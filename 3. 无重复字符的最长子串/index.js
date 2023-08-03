dac297add033d13b599e72e723126ad15c04ad0b
unknowntocak <1067075323@qq.com>
2023-08-03 20:43:42 +0800

    feat: 3.无重复字符的最长子串

---
+++ 3. 无重复字符的最长子串/index.js
@@ -0,0 +1,11 @@
+function lengthOfLongestSubstring(s) {
+  let map = new Map(), max = 0;
+  for (let i = 0, j = 0; j < s.length; j++) {
+      if (map.has(s[j])) {
+          i = Math.max(map.get(s[j]) + 1, i)
+      }
+      map.set(s[j], j)
+      max = Math.max(max, j - i + 1)
+  }
+  return max
+};
